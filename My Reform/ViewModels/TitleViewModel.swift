94e9888b39ed35a887b0418ff08ec0ed7ae072dd
Hyeonjoonn <phj3046@gmail.com>
2023-01-12 16:55:57 +0900

    0112 pbit did

---
+++ My Reform/ViewModels/TitleViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  TitleViewModel.swift
+//  My Reform
+//
+//  Created by 박현준 on 2023/01/12.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let imageURL: String // 나중에 String으로 변경 []
+    let minute: String
+    let price: String
+    let heart: Bool
+}
+
+
+
